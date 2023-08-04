f8ccd9ae9723faf0693fdf9d3faecbaf13a5893b
Song ji su <0_0727@naver.com>
2023-08-04 22:49:18 +0900

    230804

---
+++ Algorithm:DataStructure/thisiscodingtest/greedy/04.py
@@ -0,0 +1,25 @@
+n = 5
+m = 8
+k = 3
+data = [2, 4, 5, 4, 6]
+
+data.sort()
+
+
+def solution(n, m, k, data): 
+    first = data[n-1]
+    second = data[n-2]    
+    result = 0
+    while True:
+        for i in range(k):
+            if m == 0: break
+            result += first
+            m -= 1
+        if m == 0: break
+        result += second
+        m -= 1
+    print(result)
+
+solution(n, m, k, data)
+
+
