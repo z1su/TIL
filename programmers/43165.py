5223caa08936b2be046bc01222b0ed9e320c5a4a
Song ji su <0_0727@naver.com>
2023-08-04 22:49:33 +0900

    230804

---
+++ programmers/43165.py
@@ -0,0 +1,21 @@
+numbers = [1, 1, 1, 1, 1]
+target = 3
+
+def solution(numbers, target):
+    answer = DFS(numbers, target, 0)
+    return answer
+
+def DFS(numbers, target, depth):
+    answer = 0
+    if depth == len(numbers):
+        print(numbers)
+        if sum(numbers) == target:
+            return 1
+        else: return 0
+    else:
+        answer += DFS(numbers, target, depth+1)
+        numbers[depth] *= -1
+        answer += DFS(numbers, target, depth+1)
+        return answer
+DFS(numbers, target, 0)
+solution(numbers, target)
